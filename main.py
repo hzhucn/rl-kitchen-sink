678da1e2f0b3a996eb79ee3bbb2ba2a735a7eee1
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2018-07-31 16:57:01 -0400

    Add rough environment sketch

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ import argparse
 
 def parse_args():
     p = argparse.ArgumentParser(description="RL-Kitchen-Sink")
-    p.add_argument("--algorithm", default='reinforce', help='RL algorithm to use: <reinforce>')
-    p.add_argument("--env", default="CartPole-v1", help='OpenAI Gym Environment to run on')
-    p.add_argument("--mode", default="train", help='Run model in train or evaluation')
+    p.add_argument("-a", "--algorithm", default='reinforce', help='RL algorithm to use: <reinforce>')
+    p.add_argument("-e", "--env", default="CartPole-v1", help='OpenAI Gym Environment to run on')
+    p.add_argument("-d", "--distributed_backend", default="ray", help='Distributed Backend: <ray | subprocess>')
+    p.add_argument("-r", "--render", default=False, action='store_true', help='Render environment via Visdom')
+    p.add_argument("-p", "--plot", default=False, action='store_true', help='Plot summary statistics via Visdom')
 
     p.add_argument("--num_processes", default=4, help='Number of environments to run in parallel')
 
@@ -23,7 +25,7 @@ if __name__ == "__main__":
     # Start Visdom
     viz = Visdom()
 
-    # Build Environment Template
+    # Build Environment Template -> Lazy Evaluated Callable, for spawning environments
     env = build_env(args.env)
 
     # Build Distributed Environments
